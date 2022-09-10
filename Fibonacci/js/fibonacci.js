d06eb485b634fc4e0a2a46d9b3fd6705d5e61a89
Doron Barel <doron.barel@gmail.com>
2022-09-10 12:37:47 +0300

    Fibonacci - Milestone 3.1

--- Fibonacci/js/fibonacci.js
+++ Fibonacci/js/fibonacci.js
@@ -3,19 +3,18 @@ const yValue = document.getElementById("y-value");
 const submitBtn = document.getElementById("submitBtn");
 
 submitBtn.addEventListener('click', () => {
-    fibonacci(xValue.value);
+    var result = 0;
+    for(let i = 0; i <= xValue.value; i++) {
+        result = fibonacci(i);
+    }
+    yValue.innerText = result;
 });
 
 function fibonacci(number) {
-    
-    let n1 = 0,
-        n2 = 1,
-        tmp = 0;
-    
-    for (let i = 0; i < number; i++) {
-        tmp = n1 + n2;
-        n1 = n2;
-        n2 = tmp;
+    if(number < 2) {
+        return number;
     }
-    yValue.innerText = n1;
+    else {
+        return fibonacci(number-1) + fibonacci(number - 2);
+    }
 }
