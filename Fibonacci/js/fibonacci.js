4795ee592a197dc10a751bab06a5f0201351cf13
Doron Barel <doron.barel@gmail.com>
2022-09-10 12:36:19 +0300

    Fibonacci - Milestone 2

--- Fibonacci/js/fibonacci.js
+++ Fibonacci/js/fibonacci.js
@@ -1,5 +1,25 @@
 const xValue = document.getElementById("x-value");
 const yValue = document.getElementById("y-value");
 
-xValue.innerText = 8;
-yValue.innerText = 21;
+let fibOf = 8;
+
+xValue.innerText = fibOf;
+
+function fibonacci(number) {
+    
+    let n1 = 0,
+        n2 = 1,
+        tmp = 0;
+    
+    for (let i = 0; i < number; i++) {
+        tmp = n1 + n2;
+        n1 = n2;
+        n2 = tmp;
+    }
+
+    return n1;
+}
+
+let result = fibonacci(fibOf);
+
+yValue.innerText = result;
